72b5aaf47bd64d755d6b27e3ef4c03f5cb7405bc
Greg Whiteley <greg.whiteley@gmail.com>
2017-06-25 22:38:49 +1000

    Add MAP_FILE to support dumping linker map files

--- mk/skel.mk
+++ mk/skel.mk
@@ -222,8 +222,10 @@ MAKECMD.a = $(call echo_cmd,AR $@) \
 		&&) \
 	$(RANLIB) $@
 
+COMMA = ,
+
 MAKECMD.$(SOEXT) = $(LINK.cc) $(DEP_OBJS) $(DEP_ARCH) $(DEP_LIBS) $(LIBS_$(@)) $(LDLIBS) -shared -o $@
-DEFAULT_MAKECMD = $(LINK.cc) $(DEP_OBJS) $(DEP_ARCH) $(DEP_LIBS) $(LIBS_$(@)) $(LDLIBS) -o $@
+DEFAULT_MAKECMD = $(LINK.cc) $(DEP_OBJS) $(DEP_ARCH) $(DEP_LIBS) $(LIBS_$(@)) $(LDLIBS) $(if $(MAP_FILE),-Wl$(COMMA)-Map=$@.map) -o $@
 
 ########################################################################
 # Below is a "Blood sugar sex^H^H^Hmake magik" :) - don't touch it     #
