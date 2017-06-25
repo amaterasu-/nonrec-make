c65c0465e4312583b11c6fdc760e0510f0ab2e7c
Greg Whiteley <greg.whiteley@gmail.com>
2017-06-25 20:20:45 +1000

    bugfix: Deal with multi- when selecting DLL exports

--- ex1/Dir_3/dir_3.h
+++ ex1/Dir_3/dir_3.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#ifdef BUILD_ex1_Dir_3 /* export symbols if building in this directory */
+/* Note due to "dual TOP" there are multiple definitions possible here - TODO */
+#if defined(BUILD_ex1_Dir_3) || defined(BUILD_Dir_3) /* export symbols if building in this directory */
 #define DIR3_EXPORT __attribute__ ((visibility("default")))
 #else
 #define DIR3_EXPORT
