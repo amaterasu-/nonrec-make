5c685014f4c0e353bed4b1be46eb9be32617a143
Greg Whiteley <greg.whiteley@gmail.com>
2018-06-10 22:20:15 +1000

    testing-native.mk: dump output to file

--- mk/testing-native.mk
+++ mk/testing-native.mk
@@ -3,7 +3,7 @@
 # the makefiles. Change the makefiles could change the test
 
 _TESTING_FAILING_PREFIX = $(if $(filter true,$(5)),!)
-_TESTING_FAILING_SUFFIX = $(if $(filter true,$(5)), || (echo Expected \"$(2)\" to fail;false))
+_TESTING_FAILING_SUFFIX = $(if $(filter true,$(5)), || (cat $$@.out ; echo Expected \"$(2)\" to fail;false))
 _TESTING_FAILING_REASSURANCE = $(if $(filter true,$(5)),@echo \"$(2)\" failed as expected ,@:)
 
 # $(1) $(d)
@@ -14,7 +14,8 @@ _TESTING_FAILING_REASSURANCE = $(if $(filter true,$(5)),@echo \"$(2)\" failed as
 define compiled_test
 $(1)/$(OBJDIR)/$(2).test: $(1)/$(OBJDIR)/$(2) $(3) $$(MAKEFILE_DEPS_$(1)) $$(NONREC_MAKEFILES) | $(1)/$(OBJDIR)
 	@rm -f $$@
-	$$(call echo_cmd,RUN $$< $(4)) cd $$(dir $$@) && $(_TESTING_FAILING_PREFIX) $$< $(4) $(_TESTING_FAILING_SUFFIX)
+	$$(call echo_cmd,RUN $$< $(4)) cd $$(dir $$@) && $(_TESTING_FAILING_PREFIX) $$< $(4) > $$@.out $(_TESTING_FAILING_SUFFIX)
+	@cat $$@.out
 	$(_TESTING_FAILING_REASSURANCE)
 	@touch $$@
 
@@ -46,7 +47,8 @@ RUN.bats := $(or $(BATS),bats)
 define script_test
 $(1)/$(OBJDIR)/$(2).test: $(1)/$(2) $(3) $$(MAKEFILE_DEPS_$(1)) $$(NONREC_MAKEFILES) | $(1)/$(OBJDIR)
 	@rm -f $$@
-	$$(call echo_cmd,RUN $$< $(4)) cd $$(dir $$@) && $(_TESTING_FAILING_PREFIX) $(RUN$(suffix $(2))) $$< $(4) $(_TESTING_FAILING_SUFFIX)
+	$$(call echo_cmd,RUN $$< $(4)) cd $$(dir $$@) && $(_TESTING_FAILING_PREFIX) $(RUN$(suffix $(2))) $$< $(4) > $$@.out $(_TESTING_FAILING_SUFFIX)
+	@cat $$@.out
 	$(_TESTING_FAILING_REASSURANCE)
 	@touch $$@
 
